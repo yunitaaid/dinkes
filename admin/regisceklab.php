0722b4535c18480fce3b3d2566888765462796fe
yunitaaid <62281244+yunitaaid@users.noreply.github.com>
2025-07-03 13:49:26 +0700

    Update regisceklab.php

--- admin/regisceklab.php
+++ admin/regisceklab.php
@@ -170,7 +170,7 @@
           <div class="flex-none w-full max-w-full px-3">
             <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
               <div class="p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                <h6 class="dark:text-white">Daftar Registrasi Alat Kesehatan</h6>
+                <h6 class="dark:text-white">Daftar Registrasi Cek Kesehatan</h6>
               </div>
               <div class="flex-auto px-0 pt-0 pb-2">
                 <div class="p-0 overflow-x-auto">
